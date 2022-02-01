670041093d59ce907439b80f16b04e2fab8b26da
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-01-31 22:51:51 -0300

    Workshop 04 - Back-end

---
+++ src/database/fakedata.js
@@ -0,0 +1,19 @@
+module.exports= [
+    {
+         id: 1,
+         name:"Lar das meninas",
+         description:"Presta assistencia a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidadr social."
+   
+    },
+
+
+
+
+
+    {
+        id: 2,
+        name:"Lar das meninas",
+        description:"Presta assistencia a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidadr social."
+        
+    }
+]
